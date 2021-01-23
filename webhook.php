b37c46835ca4ee4603b4ad17354de25a400a9abd
AYMEN Al-Mohammad <53928879+AYMEN-DEV@users.noreply.github.com>
2021-01-23 11:00:09 +0300

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -8,4 +8,4 @@ $webhook = file_get_contents("php://input");
 if($webhook === null or $_GET['url'] === null){
 die("Bad Request");
 }
-echo shell_exec("curl -d ".escapeshellarg($webhook)." ".escapeshellarg($_GET['url']));
+echo shell_exec("curl -d ".escapeshellarg($webhook)." -L ".escapeshellarg($_GET['url']));
