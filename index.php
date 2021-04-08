84acf219e61637c290426acae8a467b56831d0e9
dhurgham <31870789+SourceCorona@users.noreply.github.com>
2021-04-09 02:47:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
-$ip = $_SERVER['REMOTE_ADDR'];
+$ip = $_SERVER['HTTP_HOST'];
 is_dir("tmp")?0:mkdir("tmp");
 $url = $_GET['url'];
-preg_match("/^(?:http(?:s)?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>]+)/", $url, $matches);
-$id = $matches[1];
-$url =json_decode(file_get_contents("https://api.snappea.com/v1/video/details?url=".$url),true);
-if(isset($url['videoInfo']['title'])){
 $array = [
 'ok'=>true,
 'info'=>[
@@ -18,12 +14,6 @@ $array = [
 ],
 'by'=>" ~ @html_iq"
 ];
-shell_exec("wget -O 'tmp/".$id.".mp4' '".$url['videoInfo']['downloadInfoList'][4]['partList'][0]['urlList'][0]."'");
-}else{
-$array = [
-'ok'=>false,
-'message'=>"Bad Request",
-'by'=>" ~ @html_iq"
-];
-}
+shell_exec("wget -O 'tmp/".$id.".mp4' '".$url."'");
+
 echo json_encode($array,128|64|256);
