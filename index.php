1ef17f991d70762c29a3925fd1325d2096b2e62f
dhurgham <31870789+SourceCorona@users.noreply.github.com>
2021-04-09 04:55:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,10 @@
 header('Content-Type: application/json; charset=utf-8');
 $ip = $_SERVER['HTTP_HOST'];
 $rand = substr(str_shuffle('AaBbCcDDEeFFGGHhIiJjKjLlMmOoPpQQRrSsTuUuVvWwXxYyZz123457890@#1'),0,4); 
-
-
 is_dir("tmp")?0:mkdir("tmp");
 $url = $_GET['url'];
 $array = [
-'ok'=>true,
-'info'=>[
 'url'=>"http://".$ip."/tmp/".$rand.".mp4",
-],
-'by'=>" ~ @html_iq"
 ];
 shell_exec("wget -O 'tmp/".$rand.".mp4' '".$url."'");
 
